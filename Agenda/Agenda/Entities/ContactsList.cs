bde10d82154872ad4acf667d35356bcb8d3eb00b
Jorge Castro <jorgevinicius9@gmail.com>
2022-04-04 22:47:45 -0300

    Agenda code

---
+++ Agenda/Agenda/Entities/ContactsList.cs
@@ -0,0 +1,62 @@
+﻿using Agenda.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Contacts.Entities
+{
+    internal class ContactsList
+    {
+        public List<Person> contacts;
+        
+        public ContactsList()
+        {
+            contacts = new List<Person>();
+        }
+
+        public void StorePerson(string name, int age, long number)
+        {
+            contacts.Add(new Person(name, age, number));
+        }
+        public void StorePerson()
+        {
+            Console.Clear();
+
+            Console.WriteLine("============ Add Contact ============");
+            Console.Write("New Contact's name: ");
+            string name = Console.ReadLine();
+            Console.Write("Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Number: ");
+            long number = long.Parse(Console.ReadLine());
+            Person p = new Person(name, age, number);
+
+            contacts.Add(p);
+        }
+
+        public void ErasePerson()
+        {
+            Console.Clear();
+
+            Console.WriteLine("============ Add Contact ============");
+            Console.Write("Contact's Name: ");
+            string name = Console.ReadLine();
+            bool removed = contacts.Remove(contacts.Find(x => x.Name == name));
+
+            if (removed) Console.WriteLine("Erased Sucessfully!!");
+            else Console.WriteLine("Contact not found!!\n");
+
+        }
+
+        public void PrintAllContacts()
+        {
+            Console.WriteLine("\n\n-====== All Contacts =====-");
+            Console.WriteLine("----------------");
+            foreach (Person p in contacts)
+            {
+                p.PrintPerson();
+                Console.WriteLine("----------------");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ Agenda/Agenda/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Contacts.Entities;
+
+
+namespace Agenda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactsList contacts = new ContactsList();
+            Console.WriteLine("============ Contact's List ============\n");
+
+            bool loop = true;
+
+            while (loop)
+            {
+                loop = Options(contacts);
+            }
+
+        }
+
+        static bool Options(ContactsList list)
+        {
+            Console.WriteLine("----------- Select an Option -----------");
+            Console.WriteLine("1- Add Contact          2- Erase Contact");
+            Console.WriteLine("3- Print All Contacts   4- Exit");
+            Console.WriteLine("----------------------------------------");
+            Console.Write("Option: ");
+            int option = int.Parse(Console.ReadLine());
+            
+            
+            switch (option)
+            {
+                case 1: list.StorePerson(); break;
+                case 2: list.ErasePerson(); break;
+                case 3: list.PrintAllContacts(); break;
+                case 4: return false;
+                default: Console.Clear(); Console.WriteLine("Invalid Command!\n"); break;
+            }
+            return true;
+        }
+    }
+}
